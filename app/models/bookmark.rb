23c3979482e0dd97796d86d4b687db50ebfee9bd
david felipe pena <david.felipe.pena@hotmail.com>
2022-11-17 16:10:37 -0300

    rails

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,7 +3,4 @@ class Bookmark < ApplicationRecord
   belongs_to :list
   validates :comment, presence: true, length: { minimum: 6 }
   validates_uniqueness_of :movie_id, scope: [:list_id]
-  has_many :movies
-  belongs_to :movie
-  belongs_to :list
 end
